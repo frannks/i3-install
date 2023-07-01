7369aa4dae37879f131b20d50934c2925eea05da
Franklin Souza <137704852+frannks@users.noreply.github.com>
2023-06-30 21:25:38 -0300

    Update i3-install.sh

--- i3-install.sh
+++ i3-install.sh
@@ -20,13 +20,13 @@ i3PATH(){
   mkdir $HOME/Scripts
   mkdir $HOME/Wallpapers
   mkdir $HOME/.projects
-  #mkdir $HOME/.config/qutebrowser
+  mkdir $HOME/.config/qutebrowser
 }
 
 # Instalação de pacotes
 packages_install(){
   #clear && sudo pacman -S archlinux-keyring --noconfirm
-  clear && sudo pacman -S arandr sxiv rustup scrot picom cronie polybar heimdall tmate meson htop unzip unrar zip p7zip nitrogen socat clamav thunderbird-i18n-pt-pt scrcpy lfs qbittorrent tor dunst fuse mpv libnotify pacman-contrib lxappearance gvfs-mtp xdg-user-dirs w3m ttf-fira-code zsh zsh-autosuggestions zsh-syntax-highlighting reflector ttf-font-awesome imagemagick inetutils bc base-devel wmctrl lsd yt-dlp bat net-tools polkit-gnome nodejs yarn fzf ueberzug dmenu man-db python-pywal wget pyenv python-pipx python-jedi python-pylint python-requests --noconfirm && xdg-user-dirs-update
+  clear && sudo pacman -S arandr sxiv rustup scrot picom cronie polybar heimdall qutebrowser tmate meson htop unzip unrar zip p7zip nitrogen socat clamav thunderbird-i18n-pt-pt scrcpy lfs qbittorrent tor dunst fuse mpv libnotify pacman-contrib lxappearance gvfs-mtp xdg-user-dirs w3m ttf-fira-code zsh zsh-autosuggestions zsh-syntax-highlighting reflector ttf-font-awesome imagemagick inetutils bc base-devel wmctrl lsd yt-dlp bat net-tools polkit-gnome nodejs yarn fzf ueberzug dmenu man-db python-pywal wget pyenv python-pipx python-jedi python-pylint python-requests --noconfirm && xdg-user-dirs-update
   rustup default stable && clear
 }
 
@@ -37,11 +37,6 @@ builds(){
   cd i3-autolayout
   makepkg -sic --noconfirm
 
-  cd $HOME/.compile
-  git clone https://aur.archlinux.org/pulseaudio-ctl.git
-  cd pulseaudio-ctl
-  makepkg -sic --noconfirm
-  
   clear
   cd $HOME/.compile
   git clone https://aur.archlinux.org/wal-telegram-git.git
@@ -53,6 +48,11 @@ builds(){
   git clone https://aur.archlinux.org/i3lock-color.git
   cd $HOME/.compile/i3lock-color
   makepkg -sic --noconfirm
+
+  #cd $HOME/.compile
+  #git clone https://aur.archlinux.org/pulseaudio-ctl.git
+  #cd pulseaudio-ctl
+  #makepkg -sic --noconfirm
 
   clear
   cd $HOME/.projects
@@ -105,7 +105,7 @@ end_script(){
   cp $HOME/.frannks-mydots/mydots/.config/polybar/config $HOME/.config/polybar
   chmod +x $HOME/.config/polybar/scripts/*
   sudo pacman -S rofi --noconfirm
-  #cp -r $HOME/.frannks-mydots/mydots/.config/qutebrowser/config.py $HOME/.config
+  cp -r $HOME/.frannks-mydots/mydots/.config/qutebrowser/config.py $HOME/.config
   cp $HOME/.frannks-mydots/mydots/.config/rofi/drun.rasi $HOME/.config/rofi/
   cp $HOME/.frannks-mydots/mydots/.config/rofi/themes/dark.rasi $HOME/.config/rofi/themes/
   cp $HOME/.frannks-mydots/mydots/.config/kitty/kitty.conf $HOME/.config/kitty
@@ -119,9 +119,9 @@ end_script(){
   sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
   cp -r $HOME/.local/share/nvim/site/autoload $HOME/.config/nvim
-  #cp $HOME/.frannks-mydots/mydots/.config/qutebrowser/* $HOME/.config/qutebrowser && cd $HOME/.config/qutebrowser && pip install -r requirements.txt && cd $HOME
+  cp $HOME/.frannks-mydots/mydots/.config/qutebrowser/* $HOME/.config/qutebrowser && cd $HOME/.config/qutebrowser && pip install -r requirements.txt && cd $HOME
 
-  #mkdir $HOME/Downloads/qute_down
+  mkdir $HOME/Downloads/qute_down
 
   clear && cd $HOME/Wallpapers
   wget -c "https://raw.githubusercontent.com/frannks/wallpapers/main/004.jpg"
