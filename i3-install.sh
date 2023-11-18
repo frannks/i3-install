cf2bb0f08758f9e7a35fa79310a628051b2844cf
Franklin Souza <137704852+frannks@users.noreply.github.com>
2023-11-18 19:33:58 -0300

    Update i3-install.sh

--- i3-install.sh
+++ i3-install.sh
@@ -38,7 +38,7 @@ i3PATH(){
 # Instalação de pacotes
 packages_install(){
   #clear && sudo pacman -S archlinux-keyring --noconfirm
-  clear && sudo pacman -S rustup kitty scrot picom cronie polybar qutebrowser slop tmate meson htop unzip unrar zip p7zip socat tor dunst fuse mpv gpick shotgun hacksaw xdotool libnotify pacman-contrib lxappearance gvfs-mtp xdg-user-dirs w3m ttf-fira-code zsh zsh-autosuggestions zsh-syntax-highlighting reflector ttf-jetbrains-mono ttf-dejavu ttf-font-awesome imagemagick inetutils bc base-devel wmctrl lsd yt-dlp bat net-tools polkit-gnome nodejs yarn fzf ueberzug dmenu man-db python-pywal wget pyenv python-pipx python-jedi python-pylint python-requests python-pyperclip --noconfirm && xdg-user-dirs-update
+  clear && sudo pacman -S rustup kitty scrot picom cronie polybar firefox-i18n-pt-br slop tmate meson htop unzip unrar zip p7zip socat tor dunst fuse mpv gpick shotgun hacksaw xdotool libnotify pacman-contrib lxappearance gvfs-mtp xdg-user-dirs w3m ttf-fira-code zsh zsh-autosuggestions zsh-syntax-highlighting reflector ttf-jetbrains-mono ttf-dejavu ttf-font-awesome imagemagick inetutils bc base-devel wmctrl lsd yt-dlp bat net-tools polkit-gnome nodejs yarn fzf ueberzug dmenu man-db python-pywal wget pyenv python-pipx python-jedi python-pylint python-requests python-pyperclip --noconfirm && xdg-user-dirs-update
   rustup default stable && clear
 }
 
